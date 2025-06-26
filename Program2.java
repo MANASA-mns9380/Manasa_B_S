3473867b29f73bf7e4895c839296eba368ea95f1
MANASA-mns9380 <mnsayadavmnsa@gmail.com>
2025-06-26 10:34:55 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,16 @@
+package classs;
+
+public class series {
+	
+	public static void main(String[] args) {
+        int a = 1;
+        		
+        for (int i = 0; i < a; i++) {
+            System.out.print(2 * i + 1);
+            if (i < a - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
+
